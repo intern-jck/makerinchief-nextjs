e2ce457d82d1fea6addf444d1d49a8c27bb75e50
Justin C Kirk <justin.c.kirk@gmail.com>
2023-06-21 21:56:00 -0400

    uses newer json

--- src/pages/kits/index.tsx
+++ src/pages/kits/index.tsx
@@ -1,12 +1,8 @@
 import Image from 'next/image'
 import Link from 'next/link';
 import { Card } from '@/common/components';
-
 import { KitType } from '@/common/types';
-// import { getJsonDataFromUrl } from '@/modules/json';
-
 import styles from './Kits.module.scss';
-
 
 // Url for products info
 const GITHUB_KITS_JSON_URL = process.env.GITHUB_KITS_JSON_URL;
@@ -33,7 +29,7 @@ export default function Kits({ kits }: Props) {
                   key={i}
                   className={styles.kitCard}
                   cardTitle={kit.name}
-                  cardImage={kit.photos[0]}
+                  cardImage={kit.photos[0].url}
                   cardText={kit.short}
                   cardLink={`/kits/${kit.slug}`}
                 />
@@ -52,7 +48,19 @@ export async function getStaticProps() {
 
   // const response = await fetch('https://raw.githubusercontent.com/intern-jck/jsons/main/kitsData2.json');
   const response = await fetch(GITHUB_KITS_JSON_URL!);
-  const kits = await response.json();
+  const data = await response.json();
+  // console.log(data.makerinchief)
+  const kits = data.makerinchief.map((item) => ({
+    slug: item.slug,
+    name: item.name,
+    short: item.short,
+    info: item.info,
+    photos: item.photos.slice(),
+    tech: item.tech.slice(),
+    repos: item.repos.slice()
+  }));
+
+  // const kits = [];
 
   return {
     props: { kits }
